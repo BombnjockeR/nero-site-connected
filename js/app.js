38e132d36b3464eb95555aeec4d9ceae96337767
BombnjockeR <81313279+BombnjockeR@users.noreply.github.com>
2026-09-16 23:20:33 +0700

    Donation: referral bonus 20% -> 10% (promo ended) in the frontend summary display

--- js/app.js
+++ js/app.js
@@ -539,12 +539,12 @@ function updateSummary(){
   var sel=document.getElementById('don-streamer');
   var code=sel.value;
   var name=code ? sel.options[sel.selectedIndex].getAttribute('data-name') : '';
-  var bonus=code?Math.round(cp*0.20):0;
+  var bonus=code?Math.round(cp*0.10):0;
   /* Same as before: base tier determines CP received. Streamer code only
      acts as a tracking tag (last 3 digits of the rupiah total). */
   var payTotal=cp+(code?Number(code):0);
   box.innerHTML='Base: <b>'+fmtNum(cp)+' CP</b><br>'+
-    (code?'Streamer bonus (+20%): <b>+'+fmtNum(bonus)+' CP</b> → '+name+' (code '+code+')<br>':'')+
+    (code?'Streamer bonus (+10%): <b>+'+fmtNum(bonus)+' CP</b> → '+name+' (code '+code+')<br>':'')+
     '<hr class="don-hr">'+
     'You receive: <b class="don-total">'+fmtNum(cp+bonus)+' CP</b><br>'+
     'You pay: <b>'+fmtRp(payTotal)+'</b>'+
